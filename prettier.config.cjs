38e65885ffa64d739543b9ab1c0d7a423cf68221
Miklos Megyes <m.miklos05@gmail.com>
2022-11-27 14:15:21 +0100

    config prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,6 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
 };
