88c2ea9ca9d3c60eef6add42738ced2008a2f661
Miklos Megyes <m.miklos05@gmail.com>
2022-12-21 23:04:44 +0100

    count tokens

--- packages/css-to-tailwind/fixtures/fine-tune/count_tokens.py
+++ packages/css-to-tailwind/fixtures/fine-tune/count_tokens.py
@@ -9,4 +9,13 @@ enc = get_encoding("gpt2")
 res = enc.encode(" ".join(sys.argv[1:]))
 
 # print the result length
-print(len(res))
+count = len(res)
+print("Token count: {}".format(count))
+print()
+
+
+print("Fine-tuning costs:")
+print("  Ada: {:.2f} USD".format(count * 0.0004 / 1000))
+print("  Babbage: {:.2f} USD".format(count * 0.0006 / 1000))
+print("  Curie: {:.2f} USD".format(count * 0.003 / 1000))
+print("  Davinci: {:.2f} USD".format(count * 0.03 / 1000))
