dfeb7b5993331dbad18069f3f3e4add0635699e2
Miklos Megyes <m.miklos05@gmail.com>
2022-12-25 00:52:39 +0100

    improve

--- packages/css-to-tailwind/fixtures/generative/scripts/update-compositions.mjs
+++ packages/css-to-tailwind/fixtures/generative/scripts/update-compositions.mjs
@@ -20,7 +20,8 @@ function range(start, end) {
 }
 
 class Bag {
-  constructor(items) {
+  constructor(name, items) {
+    this.name = name;
     this.items = new Set(items);
   }
   isEmpty() {
@@ -34,45 +35,40 @@ class Bag {
   }
 }
 
-let bags = Object.entries(data).reduce((acc, [key, value]) => {
-  if (value.length === 0) return acc;
-  return [...acc, new Bag(value)];
-}, []);
+const bags = new Set(
+  Object.entries(data).reduce((acc, [pluginName, value]) => {
+    if (value.length === 0) return acc;
+    return [...acc, new Bag(pluginName, value)];
+  }, []),
+);
 
 const result = [];
 
 while (true) {
-  const composition = [];
+  const composition = {};
   const size = choose(range(4, 8));
-  const usedBags = new Set();
 
   for (let i = 0; i < size; i++) {
-    const options = bags.filter((b) => !usedBags.has(b));
-    if (options.length === 0) break;
+    if (bags.size === 0) break;
 
-    const bag = choose(options);
-    usedBags.add(bag);
-    composition.push(bag.take());
+    const bag = choose(Array.from(bags));
+
+    composition[bag.name] = bag.take();
 
     if (bag.isEmpty()) {
-      bags = bags.filter((b) => b !== bag);
+      bags.delete(bag);
     }
   }
 
-  result.push(composition);
-  if (bags.length === 0) break;
+  // drop the less useful ones
+  if (Object.keys(composition).length >= 4) {
+    result.push(composition);
+  }
+
+  if (bags.size === 0) break;
 }
 
 await fs.writeFile(
   path.resolve(__dirname, `../compositions/compositions.json`),
   JSON.stringify(result, null, 2),
-);
-
-console.log(
-  'Compositions with only 2 items:',
-  result.filter((c) => c.length === 2).length,
-);
-console.log(
-  'Compositions with only 1 item:',
-  result.filter((c) => c.length === 1).length,
 );
