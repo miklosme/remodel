e269315df80d182ebe8f57c56fe935332c8e7ef2
Miklos Megyes <m.miklos05@gmail.com>
2022-12-25 17:24:21 +0100

    merge css

--- packages/css-to-tailwind/fixtures/generative/scripts/update-compositions-resolved.mjs
+++ packages/css-to-tailwind/fixtures/generative/scripts/update-compositions-resolved.mjs
@@ -1,11 +1,19 @@
 import path from 'path';
 import { promises as fs } from 'fs';
 import postcss from 'postcss';
+import postcssParse from 'postcss-safe-parser';
 import tailwindcss from 'tailwindcss';
 import prettier from 'prettier';
 import { URL } from 'url';
 
 const __dirname = new URL('.', import.meta.url).pathname;
+
+function formatCSS(css) {
+  return prettier.format(css, {
+    parser: 'css',
+    printWidth: 100,
+  });
+}
 
 async function resolveTailwindUtilities(utilities) {
   const config = {
@@ -22,10 +30,94 @@ async function resolveTailwindUtilities(utilities) {
     from: 'tailwind.css',
   });
 
-  return prettier.format(css, {
-    parser: 'css',
-    printWidth: 100,
+  return formatCSS(css);
+}
+
+function mergeCSSRules(css) {
+  const ast = postcssParse(css);
+  const cssData = [];
+  ast.walkRules((rule) => {
+    const data = {};
+    // debugger;
+
+    data.selector = rule.selector;
+    data.declarations = [];
+
+    rule.nodes.forEach((node) => {
+      if (node.type === 'decl') {
+        data.declarations.push([node.prop, node.value]);
+      } else {
+        throw new Error('Unexpected node type: ' + node.type);
+      }
+    });
+
+    data.atrule = null;
+
+    if (rule.parent.type === 'atrule') {
+      data.atrule = {
+        name: rule.parent.name,
+        params: rule.parent.params,
+      };
+    } else if (rule.parent.type === 'root') {
+      // ignore
+    } else {
+      debugger;
+      throw new Error('Unexpected parent type: ' + rule.parent.type);
+    }
+
+    cssData.push(data);
   });
+
+  const result = Object.entries(
+    cssData.reduce((acc, curr) => {
+      const { selector, declarations, atrule } = curr;
+
+      // TODO test selector if it's only a single class, and throw if not
+
+      if (atrule) {
+        let key = `@${atrule.name} ${atrule.params}`;
+        acc[key] = acc[key] || {
+          atrule,
+          declarations: [],
+        };
+
+        acc[key].declarations.push(...declarations);
+      } else {
+        acc['null'] = acc['null'] || {
+          atrule: null,
+          declarations: [],
+        };
+
+        acc['null'].declarations.push(...declarations);
+      }
+
+      return acc;
+    }, {}),
+  )
+    .map(([_, data]) => {
+      if (data.atrule) {
+        return `
+        @${data.atrule.name} ${data.atrule.params} {
+          .single {
+            ${data.declarations
+              .map(([prop, val]) => `  ${prop}: ${val};`)
+              .join('\n')}
+          }
+        }
+      `;
+      } else {
+        return `
+        .single {
+          ${data.declarations
+            .map(([prop, val]) => `  ${prop}: ${val};`)
+            .join('\n')}
+        }
+      `;
+      }
+    })
+    .join('\n\n');
+
+  return formatCSS(result);
 }
 
 const data = JSON.parse(
@@ -38,9 +130,11 @@ const data = JSON.parse(
 const result = await Promise.all(
   data.slice(0, 20).map(async (composition) => {
     const classList = Object.values(composition);
-    const css = await resolveTailwindUtilities(classList.join(' '));
+    const resolvedCSS = await resolveTailwindUtilities(classList.join(' '));
+    const css = await mergeCSSRules(resolvedCSS);
     return {
       classList,
+      resolvedCSS,
       css,
     };
   }),
