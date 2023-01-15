282aaca7eda5f649066efed572d41a54af28ffae
Miklos Megyes <m.miklos05@gmail.com>
2023-01-15 13:59:51 +0100

    cache resolved.json

---
+++ packages/css-to-tailwind/data/cache.mjs
@@ -0,0 +1,26 @@
+import { promises as fs } from 'fs';
+import { URL } from 'url';
+import path from 'path';
+import { entriesFromCSS } from '../src/entries.mjs';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+
+const resolvedUtilities = JSON.parse(
+  await fs.readFile(path.resolve(__dirname, 'utilities-resolved.json'), 'utf8'),
+);
+
+const results = [];
+
+for (const [utility, css] of Object.entries(resolvedUtilities)) {
+  const entries = entriesFromCSS(css);
+
+  for (const { property, value } of entries) {
+    // ['margin-left', '2.5rem', 'ml-10']
+    results.push([property, value, utility]);
+  }
+}
+
+await fs.writeFile(
+  path.resolve(__dirname, 'cache.json'),
+  JSON.stringify(results, null, 2),
+);
