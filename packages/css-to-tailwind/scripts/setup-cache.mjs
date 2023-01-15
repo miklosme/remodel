bee1dd39e5b81f19beabcd2011d1285aa9485849
Miklos Megyes <m.miklos05@gmail.com>
2023-01-15 15:14:08 +0100

    setup cache

--- packages/css-to-tailwind/scripts/setup-cache.mjs
+++ packages/css-to-tailwind/scripts/setup-cache.mjs
@@ -1,4 +1,8 @@
 import pg from 'pg';
+import chalk from 'chalk';
+import { promises as fs } from 'fs';
+import { URL } from 'url';
+import path from 'path';
 
 const client = new pg.Client();
 
@@ -12,15 +16,30 @@ await client.query(
     id SERIAL PRIMARY KEY,
     property TEXT NOT NULL,
     value TEXT NOT NULL,
-    utilities TEXT NOT NULL
+    utility TEXT NOT NULL
   )`,
 );
 
-// insert data
-await client.query(
-  `INSERT INTO tailwindcss (property, value, utilities) VALUES ($1, $2, $3)`,
-  ['margin-left', '2.5rem', 'ml-10'],
+const __dirname = new URL('.', import.meta.url).pathname;
+
+const cache = JSON.parse(
+  await fs.readFile(path.resolve(__dirname, '../data/cache.json'), 'utf8'),
 );
+
+console.log(`Now inserting ${cache.length} rows...`);
+
+for (const [property, value, utility] of cache) {
+  await client.query(
+    `INSERT INTO tailwindcss (property, value, utility) VALUES ($1, $2, $3)`,
+    [property, value, utility],
+  );
+}
+
+const {
+  rows: [{ count }],
+} = await client.query(`SELECT COUNT(*) as count FROM tailwindcss`);
+
+console.log(chalk.green(`Table size now: ${count}`));
 
 console.log('Success');
 
